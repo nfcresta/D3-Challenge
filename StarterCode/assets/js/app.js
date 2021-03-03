1e6b59906c55b2a9e39be3eef1059dfe9e080307
nfcresta <72232054+nfcresta@users.noreply.github.com>
2021-03-03 14:31:26 -0500

    chart axes

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -26,3 +26,57 @@ var chartGroup = svg.append("g")
         .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 
+// import data
+d3.csv("data.csv").then(healthData => {
+
+    console.log(healthData);
+
+    // parse data / cast as numbers
+    healthData.forEach(data => {
+        data.poverty = +data.poverty;
+        data.healthcare = +data.healthcare;
+    })
+
+    // create scale functions
+    var xLinearScale = d3.scaleLinear()
+        .domain(d3.extent(healthData, d => d.poverty))
+        .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(healthData, d => d.healthcare)])
+        .range([height, 0]);
+
+    // create axis functions
+    var xAxis = d3.axisBottom(xLinearScale);
+    var yAxis = d3.axisLeft(yLinearScale);
+
+    // append to the chart
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(xAxis);
+
+    chartGroup.append("g")
+        .call(yAxis);
+
+    // create circles for scatter plot
+    var circles = chartGroup.selectAll("circle")
+        .data(healthData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.poverty))
+        .attr("cy", d => yLinearScale(d.healthcare))
+        .attr("r", "10")
+        .attr("fill", "teal")
+        .attr("opacity", "0.8");
+
+    // create labels
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left + 40)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("Lacks Healthcare (%)");
+
+    
+});
